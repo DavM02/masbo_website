b71c7835f9530d1d2ed6c19d9af0b281283453ae
Dav <dmatosyan2002@gmail.com>
2025-02-26 15:00:39 +0400

    upd

--- src/app.jsx
+++ src/app.jsx
@@ -3,7 +3,7 @@ import Header from '@components/layout/Header/Header'
 import { AnimatePresence } from 'framer-motion'
 import { Routes, Route, useLocation} from 'react-router-dom'
 import SmoothScroll from '@components/Scroll/SmoothScroll'
- import Footer from '@components/layout/Footer/Footer'
+import Footer from '@components/layout/Footer/Footer'
 
 export function App() {
  
@@ -11,31 +11,30 @@ export function App() {
 
   return (
     <>
-   
+  
+      <Header />
 
- 
-        <Header />
-
-        <SmoothScroll>
+      <SmoothScroll>
           
-          <main>
+        <main>
 
-            <AnimatePresence
-              mode="wait"
-              initial={false}>
-              <Routes
-                location={location}
-                key={location.pathname}>
-                <Route
-                  path="/"
-                  element={<HomePage />} />
-              </Routes>
+          <AnimatePresence
+            mode="wait"
+            initial={false}>
+            <Routes
+              location={location}
+              key={location.pathname}>
+              <Route
+                path="/"
+                element={<HomePage />} />
+            </Routes>
 
-            </AnimatePresence>
+          </AnimatePresence>
     
-          </main>
- <Footer/>
-        </SmoothScroll>
+        </main>
+
+        <Footer />
+      </SmoothScroll>
  
     </>
   )
