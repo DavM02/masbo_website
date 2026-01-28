058055dead838391c160983b74906310ca7191c3
Dav <dmatosyan2002@gmail.com>
2026-01-28 18:38:36 +0400

    upd

--- src/pages/TeamPage/TeamPage.jsx
+++ src/pages/TeamPage/TeamPage.jsx
@@ -1,23 +1,45 @@
-import PageTransition from "@components/PageTransition/PageTransition" 
+// import PageTransition from "@components/PageTransition/PageTransition" 
+// import Loading from "@components/shared/Loading/Loading";
+
+// import { lazy, Suspense } from "preact/compat"
+// import { LazyLoadComponent } from "react-lazy-load-image-component";
+// const TeamSections = lazy(() => import("./TeamSections"));
+
+// function TeamPage() {
+
+//   return (
+//     <div
+//       id='team-page'> 
+//       <Suspense
+        
+//         fallback={    <Loading />}>
+//         <TeamSections />
+//       </Suspense>
+  
+//     </div>
+//   )
+// }
+
+// export default PageTransition(TeamPage)
+
+
+// TeamPage.jsx
+import PageTransition from "@components/PageTransition/PageTransition";
+import loadable from "@loadable/component";
 import Loading from "@components/shared/Loading/Loading";
 
-import { lazy, Suspense } from "preact/compat"
-
-const TeamSections = lazy(() => import("./TeamSections"));
+// ленивый импорт секций
+const TeamSections = loadable(() => import("./TeamSections"), {
+  fallback: <Loading /> // блок, который займет место вместо мерцания
+});
 
 function TeamPage() {
-
   return (
     <div
-      id='team-page'> 
-      <Suspense
-        
-        fallback={    <Loading />}>
-        <TeamSections />
-      </Suspense>
-  
+      id="team-page">
+      <TeamSections />
     </div>
-  )
+  );
 }
 
-export default PageTransition(TeamPage)
+export default PageTransition(TeamPage);
