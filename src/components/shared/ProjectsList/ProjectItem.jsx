b78d757e87117d784ca3b29a8c50df2014f0b065
Dav <dmatosyan2002@gmail.com>
2026-02-01 01:22:35 +0400

    upd

--- src/components/shared/ProjectsList/ProjectItem.jsx
+++ src/components/shared/ProjectsList/ProjectItem.jsx
@@ -1,28 +1,31 @@
 import { LazyLoadImage } from "react-lazy-load-image-component"
+import { Link } from "react-router-dom"
 export default function ProjectItem({el}) {
   return (
     <li>
-      <div
-        className='placeholder-1'>
-        <LazyLoadImage
-          src={`${import.meta.env.BASE_URL}${el.image}`}
-          width={"100%"}
-          height={"127px"}
-          effect="opacity"
-          alt={'project-image'} />
-      </div>
+      <Link
+        to={`/news/blog-${el.id}`}>
+        <div
+          className='placeholder-1'>
+          <LazyLoadImage
+            src={`${import.meta.env.BASE_URL}${el.image}`}
+            width={"100%"}
+            height={"127px"}
+            effect="opacity"
+            alt={'project-image'} />
+        </div>
 
-      <span
-        className='fs-12 up-case'>
-        {el.date}
-      </span>
+        <span
+          className='fs-12 up-case'>
+          {el.date}
+        </span>
 
-      <h5>{el.title}</h5>
+        <h5>{el.title}</h5>
 
-      <p
-        className='to-middle capitalize'>
-        {el.text}...
-      </p>
+        <p
+          className='to-middle capitalize'>
+          {el.text}...
+        </p></Link>
     </li> 
   )
 }
