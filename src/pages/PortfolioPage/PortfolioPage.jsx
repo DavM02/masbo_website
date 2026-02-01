e71ba9940bca66e5a9f0712fc303b2a8b1ff39f1
Dav <dmatosyan2002@gmail.com>
2026-02-01 12:07:18 +0400

    upd

--- src/pages/PortfolioPage/PortfolioPage.jsx
+++ src/pages/PortfolioPage/PortfolioPage.jsx
@@ -1,14 +1,14 @@
 import { useLazyComponent } from "@hooks/useLazyComponent";
 import Loading from "@components/shared/Loading/Loading";
-
+import Portfolio from "../../components/sections/Portfolio/Portfolio";
 export default function PortfolioPage() {
 
-  const PortfolioSections = useLazyComponent(() => import("@components/sections/Portfolio/Portfolio"));
+  // const PortfolioSections = useLazyComponent(() => import("@components/sections/Portfolio/Portfolio"));
 
 
   return (<>
     
-    {PortfolioSections ? <PortfolioSections  /> : <Loading />}
-    
+    {/* {PortfolioSections ? <PortfolioSections  /> : <Loading />} */}
+    <Portfolio />     
   </>) 
 }
