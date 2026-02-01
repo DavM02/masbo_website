34631791da44a0570f9a5eb00a79f2da99c14105
Dav <dmatosyan2002@gmail.com>
2026-02-01 12:17:41 +0400

    upd

--- src/pages/PortfolioPage/PortfolioRoute.jsx
+++ src/pages/PortfolioPage/PortfolioRoute.jsx
@@ -2,11 +2,11 @@ import SmoothAppearance from '@ui/SmoothAppearance';
 import PageTransition from "@components/PageTransition/PageTransition";
 import { AnimatePresence } from 'framer-motion';
 import { Routes, Route, useLocation } from "react-router-dom";
-import { getScrollBar } from "@components/Scroll/ScrollAccess";
-import { ScrollTrigger } from "gsap/ScrollTrigger";
+// import { getScrollBar } from "@components/Scroll/ScrollAccess";
+// import { ScrollTrigger } from "gsap/ScrollTrigger";
 
-import PortfolioHeading from "@components/sections/PortfolioHeading/PortfolioHeading"
-import PortfolioInsidePage from './PortfolioInsidePage/PortfolioInsidePage';
+// import PortfolioHeading from "@components/sections/PortfolioHeading/PortfolioHeading"
+// import PortfolioInsidePage from './PortfolioInsidePage/PortfolioInsidePage';
 import PortfolioPage from './PortfolioPage';
 
 function PortfolioRoute() {
@@ -20,58 +20,58 @@ function PortfolioRoute() {
   return (
     <div
       id="portfolio-page">
-      <PortfolioHeading />
+      {/* <PortfolioHeading /> */}
 
       <AnimatePresence
       
         
-        onExitComplete={() => {
+        // onExitComplete={() => {
 
         
-          ScrollTrigger.getAll().forEach(trigger => {
-            if (
-              trigger.vars.id !== "header_trigger_1" 
-            ) {
-              trigger.kill();
-            }
-          });
+        //   ScrollTrigger.getAll().forEach(trigger => {
+        //     if (
+        //       trigger.vars.id !== "header_trigger_1" 
+        //     ) {
+        //       trigger.kill();
+        //     }
+        //   });
 
-          if (!window.location.hash.includes('portfolio')) return;
+        //   if (!window.location.hash.includes('portfolio')) return;
 
-          const scrollbar = getScrollBar();
+        //   const scrollbar = getScrollBar();
 
-          if (scrollbar) {
-            scrollbar.scrollTo(0, 375, 0);
-          } else {
-            window.scrollTo({
-              top: 375,
-              left: 0,
-              behavior: 'auto',
-            });
+        //   if (scrollbar) {
+        //     scrollbar.scrollTo(0, 375, 0);
+        //   } else {
+        //     window.scrollTo({
+        //       top: 375,
+        //       left: 0,
+        //       behavior: 'auto',
+        //     });
 
-          }
+        //   }
  
-        }}
+        // }}
 
 
         mode="wait">
         <SmoothAppearance
-          onAnimationStart={(e) => {
-            if (e.opacity !== 0) return
+          // onAnimationStart={(e) => {
+          //   if (e.opacity !== 0) return
  
-            getScrollBar()?.updatePluginOptions("overflow", {
-              open: true,
-            });
+          //   getScrollBar()?.updatePluginOptions("overflow", {
+          //     open: true,
+          //   });
  
-          }}
-          onAnimationComplete={(e) => {
-            if (e.opacity !== 1) return
+          // }}
+          // onAnimationComplete={(e) => {
+          //   if (e.opacity !== 1) return
        
-            getScrollBar()?.updatePluginOptions("overflow", {
-              open: false,
-            });
+          //   getScrollBar()?.updatePluginOptions("overflow", {
+          //     open: false,
+          //   });
 
-          }}
+          // }}
 
           id="portfolio-page-transition"
           updateLayout={true}
@@ -84,9 +84,9 @@ function PortfolioRoute() {
               index
               element={ <PortfolioPage />} />
 
-            <Route
+            {/* <Route
               path=":slug"
-              element={<PortfolioInsidePage />} />
+              element={<PortfolioInsidePage />} /> */}
           </Routes>
         </SmoothAppearance>
       </AnimatePresence>
