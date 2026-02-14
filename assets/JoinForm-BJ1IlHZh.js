1ba6df91983a8a60f8c9f08cce6963a2d03a8eb7
Dav <dmatosyan2002@gmail.com>
2026-02-14 14:41:12 +0400

    Updates

---
+++ assets/JoinForm-BJ1IlHZh.js
@@ -0,0 +1,1 @@
+import{a as i,F as n}from"./index-CUAe1hR0.js";function o(){return i("section",{id:"join-form",children:i("div",{className:"container",children:i(n,{})})})}export{o as J};
--- assets/JoinForm-BO9Em1sD.js
+++
@@ -1,1 +0,0 @@
-import{a as i,F as n}from"./index-C8-Yi3iO.js";function o(){return i("section",{id:"join-form",children:i("div",{className:"container",children:i(n,{})})})}export{o as J};
