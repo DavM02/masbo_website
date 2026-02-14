8b7c4fec7ab976b22500148e78c7648eda6658c4
Dav <dmatosyan2002@gmail.com>
2026-02-14 14:14:25 +0400

    Updates

--- assets/SectionHeader-CQf5Cze0.js
+++
@@ -1,1 +0,0 @@
-import{a as e}from"./index-T5VQu_am.js";function n({headline:a,num:s}){return e("div",{className:"section-header row s-between",children:[e("span",{className:"fs-14 text-black up-case",children:a}),e("span",{className:"fs-14 text-black up-case",children:["0",s]})]})}export{n as S};
